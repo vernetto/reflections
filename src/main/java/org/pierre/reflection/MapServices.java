902af8ceb0c212891acdcd550df8c5d1521d599b
vernetto <publicpierre@gmail.com>
2022-09-23 18:52:46 +0200

    add

--- src/main/java/org/pierre/reflection/MapServices.java
+++ src/main/java/org/pierre/reflection/MapServices.java
@@ -5,10 +5,14 @@ import org.reflections.Reflections;
 
 import java.util.Set;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+
 public class MapServices {
     public static void main(String[] args) {
         Reflections reflections = new Reflections("org.pierre.reflection.services");
         Set<Class<? extends Service>> services = reflections.getSubTypesOf(Service.class);
-        System.out.printf("services=" + services);
+        System.out.println("all subtypes (interfaces and implementations) =" + services);
+        services.stream().filter(aClass -> !aClass.isInterface()).forEach(aClass -> System.out.println("Service implementation " + aClass));
+
     }
 }
