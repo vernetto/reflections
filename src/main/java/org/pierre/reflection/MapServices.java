5dd1af2d8f65c1bd66d3e970b107a286eab35416
vernetto <publicpierre@gmail.com>
2022-09-23 20:06:30 +0200

    add

--- src/main/java/org/pierre/reflection/MapServices.java
+++ src/main/java/org/pierre/reflection/MapServices.java
@@ -3,7 +3,11 @@ package org.pierre.reflection;
 import org.pierre.reflection.services.Service;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.reflections.scanners.Scanners.SubTypes;
 
@@ -12,7 +16,21 @@ public class MapServices {
         Reflections reflections = new Reflections("org.pierre.reflection.services");
         Set<Class<? extends Service>> services = reflections.getSubTypesOf(Service.class);
         System.out.println("all subtypes (interfaces and implementations) =" + services);
-        services.stream().filter(aClass -> !aClass.isInterface()).forEach(aClass -> System.out.println("Service implementation " + aClass));
-
+        List<Class<? extends Service>> allServiceImpl = services.stream().filter(aClass -> !aClass.isInterface()).collect(Collectors.toList());
+        System.out.println("service implementations = " + allServiceImpl);
+        for (Class<? extends Service> serviceImpl : allServiceImpl) {
+            Field[] allFields = serviceImpl.getDeclaredFields();
+            for (Field field : allFields) {
+                if (isService(field)) {
+                    System.out.println("service " + serviceImpl + " uses a member of interface : " + field.getType());
+                }
+            }
+        }
     }
+
+    private static boolean isService(Field field) {
+        return Arrays.stream(field.getType().getInterfaces()).filter(aClass -> aClass == Service.class).count() > 0;
+    }
+
+
 }
--- src/main/java/org/pierre/reflection/services/Service1Impl.java
+++ src/main/java/org/pierre/reflection/services/Service1Impl.java
@@ -3,4 +3,5 @@ package org.pierre.reflection.services;
 public class Service1Impl implements  Service1 {
     private Service2 service2;
     private Service3 service3;
+    private String notAService;
 }
