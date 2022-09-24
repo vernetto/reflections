ce9638687f17d11a305a4ee02c8cdad633f4d5d7
vernetto <publicpierre@gmail.com>
2022-09-24 13:33:31 +0200

    add

--- src/main/java/org/pierre/reflection/MapServices.java
+++ src/main/java/org/pierre/reflection/MapServices.java
@@ -4,19 +4,17 @@ import org.pierre.reflection.services.Service;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
-
-import static org.reflections.scanners.Scanners.SubTypes;
 
 public class MapServices {
     public static void main(String[] args) {
         Reflections reflections = new Reflections("org.pierre.reflection.services");
         Set<Class<? extends Service>> services = reflections.getSubTypesOf(Service.class);
         System.out.println("all subtypes (interfaces and implementations) =" + services);
+
         List<Class<? extends Service>> allServiceImpl = services.stream().filter(aClass -> !aClass.isInterface()).collect(Collectors.toList());
+        allServiceImpl.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
         System.out.println("service implementations = " + allServiceImpl);
         for (Class<? extends Service> serviceImpl : allServiceImpl) {
             Field[] allFields = serviceImpl.getDeclaredFields();
